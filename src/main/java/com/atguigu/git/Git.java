027ad63e6d8309b18c72a38675808f9b5ee9c3fb
Mr Yang <1250456532@qq.com>
2022-05-01 16:22:56 +0800

    12

--- src/main/java/com/atguigu/git/Git.java
+++ src/main/java/com/atguigu/git/Git.java
@@ -16,7 +16,9 @@ public class Git {
         System.out.println("hello git5");
         System.out.println("hello git5");
         System.out.println("hello git4");
- System.out.println("hello git4111");
+        System.out.println("hello git4111");
+        System.out.println("hello git4111");
+        System.out.println("hello git4111");
 
     }
 }
