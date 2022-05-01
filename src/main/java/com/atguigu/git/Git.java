71c0d951c7264a7020dc8bd2bf73fe2ae0bb36ad
Mr Yang <1250456532@qq.com>
2022-05-01 15:12:37 +0800

    second comit

--- src/main/java/com/atguigu/git/Git.java
+++ src/main/java/com/atguigu/git/Git.java
@@ -10,7 +10,8 @@ public class Git {
     public static void main (String[] args) {
         System.out.println("hello git");
         System.out.println("hello git2");
-        System.out.println("hello git3");
 
+        System.out.println("hello git3");
+        System.out.println("hello git5");
     }
 }
